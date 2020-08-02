e1c64b14cec01eab1819c88331690454e7870fec
Crystal <crystalwatkins428@gmail.com>
2020-08-02 18:05:19 -0400

    Updates

---
+++ precache-manifest.b2cfce253c5c8cee3b5a795a5f1d05a6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48c3532d7ec9e1f225f93b077ab09c29",
+    "url": "/gt-hw-react-user-directory-app/index.html"
+  },
+  {
+    "revision": "642071156c81f9716137",
+    "url": "/gt-hw-react-user-directory-app/static/js/2.1559cf65.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gt-hw-react-user-directory-app/static/js/2.1559cf65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27f0ba86ffb83aa7f454",
+    "url": "/gt-hw-react-user-directory-app/static/js/main.577467c7.chunk.js"
+  },
+  {
+    "revision": "c05460240c2780d6655f",
+    "url": "/gt-hw-react-user-directory-app/static/js/runtime-main.4d50c99a.js"
+  }
+]);
