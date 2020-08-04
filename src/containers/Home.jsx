6ff55d7ab59241c41d075e0adaf1b5c78ab907f4
Crystal <crystalwatkins428@gmail.com>
2020-08-03 23:33:31 -0400

    updated readme and pictures

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -11,7 +11,7 @@ class Home extends Component {
   };
 
   componentDidMount() {
-    Axios.get("https://randomuser.me/api/?results=10").then((res) =>
+    Axios.get("https://randomuser.me/api/?results=50").then((res) =>
 
       this.setState({
         employees: res.data.results,
@@ -28,7 +28,7 @@ class Home extends Component {
 
   handleSearch  = () => {
     console.log(this.state.search)
-    Axios.get("https://randomuser.me/api/?name" + this.state.search).then((response) => {
+    Axios.get("https://randomuser.me/api/?name=" + this.state.search).then((response) => {
       console.log(response)
       this.setState({employees: response.data.results})
     })
